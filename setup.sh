bc1b34d059a0ff5f496af0be256687fee8ffeb8b
Tom Palmer <remlapmot@hotmail.com>
2025-08-09 11:45:41 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ rm -rf .gitignore .python-version main.py pyproject.toml uv.lock .venv
 uv init --python 3.13
 #uv add nbstata ipykernel pyyaml nbformat nbclient
 uv add nbstata jupyter
-export PYTHONPATH=/Applications/Stata/utilities
+# export PYTHONPATH=/Applications/Stata/utilities
 source .venv/bin/activate
 python -m nbstata.install
 quarto render index.qmd --execute-daemon-restart
